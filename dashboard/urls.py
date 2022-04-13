a7d6b73c6fcd38fbc9e3304ba2cb6b709a7b2bbf
williamjbf <williamjbf@gmail.com>
2022-04-13 12:04:42 -0300

    created home page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from dashboard import views
+
+urlpatterns = [
+    path('', views.home)
 ]
--- system/settings.py
+++ system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
